24907cacc88d97e3a23cd333663a82e928f1324f
Rharison Lucas <rharison.abreu@gmail.com>
2022-07-01 14:36:19 -0300

    Init server http with lib express.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express, { NextFunction, Request as ExpressRequest, Response } from 'express'
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.disable('x-powered-by').disable('etag')
+app.listen(3333, () => {
+  console.log(`Server listening on port ${3333}`)
+})
+
+app.get('/', async (req: ExpressRequest, res: Response) => {
+  res.send('Hello World!')
+})
